cfa415cf2b3b4be0cd67ef54317526e48163e88f
Adam <117304208+Attentus99@users.noreply.github.com>
2025-12-05 12:15:28 +0100

    Add files via upload

---
+++ Zad3.py
@@ -0,0 +1,26 @@
+﻿# Zliczanie liczby samogłosek i spółgłosek w zadanym ciągu
+
+
+# %%
+# Utwórz zmienne "vowels" (samogłoski) i "consonants" (spółgłoski) i przypisz każdej z nich wartość 0
+
+vowels = 0
+consonants = 0
+
+# Utwórz pętlę i przeiteruj łańcuch znaków „Programowanie Pythona”
+
+name = "Programowanie Pythona"
+
+# Utwórz instrukcję warunkową IF-ELSE, która wyliczy liczbę samogłosek i spółgłosek w danym łańcuchu znaków
+
+for char in name.lower():
+    if char.isalpha():
+        if char in "aeiouyąęó":
+            vowels += 1
+        else:
+            consonants += 1
+
+# Wydrukuj łączną liczbę samogłosek i spółgłosek w danym łańcuchu znaków
+
+print("Liczba samoglosek:", vowels)
+print("Liczba spolglosek:", consonants)
