14178173f4f196c3c6d7240be9f4b8c224487c96
Adam <117304208+Attentus99@users.noreply.github.com>
2025-12-20 12:27:25 +0100

    Add files via upload

---
+++ Zad4.py
@@ -0,0 +1,17 @@
+﻿import random
+questions = [
+    "Why is the sky blue?",
+    "Why is the sun round?",
+    "Where are all the dinosaurs?"
+]
+answer = ""
+
+while answer != "to wszystko":
+    question = random.choice(questions)
+
+    answer = input(question + "\n> ")
+
+    if answer != "to wszystko":
+        print("dlaczego?\n")
+
+print("\nkoniec")
